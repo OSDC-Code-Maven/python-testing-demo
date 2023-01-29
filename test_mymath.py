584261b81ab66bc1c0810023ea576b0002ab9d12
Gabor Szabo <gabor@szabgab.com>
2023-01-29 16:44:22 +0200

    add more tests

--- test_mymath.py
+++ test_mymath.py
@@ -1,5 +1,14 @@
 import mymath
 
+
+#def test_zero():
+#    assert mymath.add(0, 0) == 0
+
+
+#def test_add_again():
+#    assert mymath.add(3, 3) == 6
+
 def test_add():
     assert mymath.add(2, 2) == 4
+
 
